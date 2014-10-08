2dbe88fbd3f820c2b30ab59da8a5a871ec684b08
nicolehunnicutt <nicolehunnicutt@students.abtech.edu>
2014-10-08 19:27:46 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chap7Hunnicutt::Application.config.secret_token = '7fdf636b6001c711c79a5b7c3d50ab8c69509f88191d6f2b283bfa0ceb635d0bc9f6feceec3cd5e86ea8d84a24d76fedeb81177e7ea6d22fa2631b208d74597f'
---
+++ db/migrate/20141008224946_create_students.rb
@@ -0,0 +1,16 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :address_1
+      t.string :address_2
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
